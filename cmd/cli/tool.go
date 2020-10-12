2aaf0b805c46cd73ec903f10c8a45a8472dd4964
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-12 19:50:43 -0400

    added application entry point

--- cmd/cli/tool.go
+++ cmd/cli/tool.go
@@ -1,7 +1,21 @@
 package main
 
-import ( "fmt" )
+import ( 
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/urfave/cli" 
+)
 
 func main() {
-	fmt.Println("Go CLI v.0.01")
+	app := cli.NewApp()
+	app.Name = "Web Query CLII"
+	app.Usage = "query  IPs, CNAMEs, MX records and Name Servers!"
+	
+	err := cli.NewApp().Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
