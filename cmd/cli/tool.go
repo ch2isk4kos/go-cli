df7e427a6fdaec0be5c2b83a794b3a286caad50a
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-12 23:58:19 -0400

    tested application entry point

--- cmd/cli/tool.go
+++ cmd/cli/tool.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/urfave/cli/v2"
 )
 
-func main() {	
-	(&cli.App{}).Run(os.Args)
+func main() {
+	app := &cli.App{
+		Name: "test",
+		Usage: "a test command",
+		Action: func(c *cli.Context) error {
+			fmt.Println("Testing...Testing...")
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	} 
 }
