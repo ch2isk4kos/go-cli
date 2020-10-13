268e1a4306f2e4771f153cb27a27c6deda617c13
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-12 20:12:41 -0400

    created a flag for host argument

--- cmd/cli/tool.go
+++ cmd/cli/tool.go
@@ -14,6 +14,13 @@ func main() {
 	app.Name = "Web Query CLII"
 	app.Usage = "query  IPs, CNAMEs, MX records and Name Servers!"
 	
+	fs := []cli.Flag{
+		cli.StringFlag{
+			Name: "host"
+			Value: "Chris Kakos"
+		}
+	}
+	
 	err := cli.NewApp().Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
