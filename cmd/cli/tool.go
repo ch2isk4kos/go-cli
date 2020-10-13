44c6e51383d176e898a12792b9a9b6d573d8fe3b
ch2isk4kos <ch2isk4kos@gmail.com>
2020-10-12 23:30:29 -0400

    initialized module

--- cmd/cli/tool.go
+++ cmd/cli/tool.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
 	fmt.Println("Oh, hello")
+	
+	(&cli.App{}).Run(os.Args)
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ch2isk4kos/go-cli
+
+go 1.15
+
+require github.com/urfave/cli/v2 v2.2.0 // indirect
